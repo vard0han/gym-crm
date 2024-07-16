60a0632eb54e6dda6af01846898c2916aabeed1d
vard0han <vdavi21@freeuni.edu.ge>
2024-07-16 20:00:43 +0400

    added loggers

--- src/main/java/com/example/Dao/DaoImpl/TraineeDaoImpl.java
+++ src/main/java/com/example/Dao/DaoImpl/TraineeDaoImpl.java
@@ -3,6 +3,8 @@ package com.example.Dao.DaoImpl;
 import com.example.Dao.InMemoryStorage;
 import com.example.Dao.UserDao;
 import com.example.model.Trainee;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -12,35 +14,50 @@ import java.util.stream.Collectors;
 
 @Repository
 public class TraineeDaoImpl implements UserDao<Trainee> {
+    private static final Logger logger = LoggerFactory.getLogger(TraineeDaoImpl.class);
+
     @Autowired
     private InMemoryStorage storage;
+
     @Override
     public void create(Trainee user) {
-        Map<String, Object> trainees =  storage.getNamespace("trainee");
-        trainees.put(user.getUsername(),user);
+        logger.debug("Creating trainee: {}", user.getUsername());
+        Map<String, Object> trainees = storage.getNamespace("trainee");
+        trainees.put(user.getUsername(), user);
+        logger.info("Trainee created: {}", user.getUsername());
     }
 
     @Override
     public void update(Trainee user) {
-        Map<String, Object> trainees =  storage.getNamespace("trainee");
-        trainees.put(user.getUsername(),user);
+        logger.debug("Updating trainee: {}", user.getUsername());
+        Map<String, Object> trainees = storage.getNamespace("trainee");
+        trainees.put(user.getUsername(), user);
+        logger.info("Trainee updated: {}", user.getUsername());
     }
 
     @Override
     public void delete(String username) {
-        Map<String, Object> trainees =  storage.getNamespace("trainee");
+        logger.debug("Deleting trainee: {}", username);
+        Map<String, Object> trainees = storage.getNamespace("trainee");
         trainees.remove(username);
+        logger.info("Trainee deleted: {}", username);
     }
 
     @Override
     public Trainee select(String username) {
-        Map<String, Object> trainees =  storage.getNamespace("trainee");
-        return (Trainee) trainees.get(username);
+        logger.debug("Selecting trainee: {}", username);
+        Map<String, Object> trainees = storage.getNamespace("trainee");
+        Trainee trainee = (Trainee) trainees.get(username);
+        logger.debug("Selected trainee: {}", trainee);
+        return trainee;
     }
 
     @Override
     public List<Trainee> selectAll() {
-        Map<String, Object> trainees =  storage.getNamespace("trainee");
-        return trainees.values().stream().map(t -> (Trainee) t).collect(Collectors.toList());
+        logger.debug("Selecting all trainees");
+        Map<String, Object> trainees = storage.getNamespace("trainee");
+        List<Trainee> traineeList = trainees.values().stream().map(t -> (Trainee) t).collect(Collectors.toList());
+        logger.debug("Selected all trainees: {}", traineeList);
+        return traineeList;
     }
 }
--- src/main/java/com/example/Dao/DaoImpl/TrainingDaoImpl.java
+++ src/main/java/com/example/Dao/DaoImpl/TrainingDaoImpl.java
@@ -3,6 +3,8 @@ package com.example.Dao.DaoImpl;
 import com.example.Dao.InMemoryStorage;
 import com.example.Dao.TrainingDao;
 import com.example.model.Training;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -12,23 +14,34 @@ import java.util.stream.Collectors;
 
 @Repository
 public class TrainingDaoImpl implements TrainingDao {
+    private static final Logger logger = LoggerFactory.getLogger(TrainingDaoImpl.class);
+
     @Autowired
     private InMemoryStorage storage;
+
     @Override
     public void create(Training training) {
-        Map<String, Object> trainings =  storage.getNamespace("training");
-        trainings.put(training.getTrainingName(),trainings);
+        logger.debug("Creating training: {}", training.getTrainingName());
+        Map<String, Object> trainings = storage.getNamespace("training");
+        trainings.put(training.getTrainingName(), training);
+        logger.info("Training created: {}", training.getTrainingName());
     }
 
     @Override
     public Training select(String trainingName) {
-        Map<String, Object> trainings =  storage.getNamespace("training");
-        return (Training) trainings.get(trainingName);
+        logger.debug("Selecting training: {}", trainingName);
+        Map<String, Object> trainings = storage.getNamespace("training");
+        Training training = (Training) trainings.get(trainingName);
+        logger.debug("Selected training: {}", training);
+        return training;
     }
 
     @Override
     public List<Training> selectAll() {
-        Map<String, Object> trainings =  storage.getNamespace("training");
-        return trainings.values().stream().map(t-> (Training) t).collect(Collectors.toList());
+        logger.debug("Selecting all trainings");
+        Map<String, Object> trainings = storage.getNamespace("training");
+        List<Training> trainingList = trainings.values().stream().map(t -> (Training) t).collect(Collectors.toList());
+        logger.debug("Selected all trainings: {}", trainingList);
+        return trainingList;
     }
 }
